883a897bc8bcfd22845e3b0e98f1e0fec37656a6
Hamanovich Siarhei <hamanovich@gmail.com>
2014-12-03 17:54:00 +0300

    Add more examples, countup with lines, version 1.3

--- jquery.hsCounter.js
+++ jquery.hsCounter.js
@@ -1,32 +1,28 @@
 /*!
- * jquery.hsCounter.js 1.0
+ * jquery.hsCounter.js 1.3
  *
- * Copyright 2014, Siarhei Hamanovich http://hamanovich.ru @hamanovich
+ * Copyright 2014, Siarhei Hamanovich http://2i.by @hamanovich
  * Released under the GPL v2 License
  *
- * Date: Oct 8, 2014
+ * Date: Dec 3, 2014
  */
 
-(function($) {
+(function ($) {
     "use strict";
-
     $.fn.hsCounter = function (options) {
-
         // Defaults
         var settings = $.extend({
-            delay: 50,                   // timeout between iterations
-            signPos: 'after',            // sign position 'before' or 'after'
+            delay: 50, // timeout between iterations
+            signPos: 'after', // sign position 'before' or 'after'
             classVisible: 'countup-vis', // name of class when counter is visible on screen
-            decimalSeparator: '.',       // decimal separator
-            orderSeparator: ' '          // order number separator
+            decimalSeparator: '.', // decimal separator
+            orderSeparator: ' ' // order number separator
         }, options);
 
         return this.each(function () {
-
             if (options) {
                 $.extend(settings, options);
             }
-
             var self = $(this),
                 win = $(window),
                 winTop = win.scrollTop(),
@@ -35,37 +31,50 @@
                 increment = self.data('increment'),
                 fractional = self.data('fractional') ? self.data('fractional') : 0,
                 sign = self.data('sign') ? self.data('sign') : '',
+                regExp = /(\d)(?=(\d\d\d)+([^\d]|$))/g,
+                defHtml = self.html(),
                 i = 0,
                 timer,
                 num,
-                regExp = /(\d)(?=(\d\d\d)+([^\d]|$))/g;
+                ins,
+                line;
 
-                if (winTop <= self.offset().top && (winTop + winHeight) >= self.offset().top
-                    && !self.hasClass(settings.classVisible)) {
+            if (winTop <= self.offset().top && (winTop + winHeight) >= self.offset().top && !self.hasClass(settings.classVisible)) {
+                timer = setTimeout(function run() {
 
-                    timer = setTimeout(function run() {
+                    if (i < numb) {
+                        i += increment;
+                    } else {
+                        i = numb;
+                    }
 
-                        if (i < numb) {
-                            i += increment;
-                        } else {
-                            i = numb;
-                        }
+                    num = i.toFixed(fractional)
+                        .replace('.', settings.decimalSeparator)
+                        .replace(regExp, '$1' + settings.orderSeparator);
 
-                        num = i.toFixed(fractional).replace('.', settings.decimalSeparator).replace(regExp, '$1'+settings.orderSeparator);
+                    if (settings.signPos == 'after') {
+                        self.html(num + '&nbsp;' + '<span class="countup-sign">' + sign + '</span>' + defHtml);
 
-                        if (settings.signPos == 'after'){
-                            self.html(num + '&nbsp;' + '<span class="countup-sign">' + sign + '</span>');
-                        } else if (settings.signPos == 'before') {
-                            self.html('<span class="countup-sign">' + sign + '</span>' + '&nbsp;' + num);
-                        }
+                    } else if (settings.signPos == 'before') {
+                        self.html('<span class="countup-sign">' + sign + '</span>' + '&nbsp;' + num + defHtml);
+                    }
 
-                        timer = setTimeout(run, settings.delay);
+                    if (self.hasClass('progress-up')) {
+                        ins = self.find('ins');
+                        ins.css('width', i + '%');
+                    }
 
-                    }, settings.delay);
+                    if (self.parent().hasClass('countup-wrap')) {
+                        line = self.parent().find('.countup-line');
+                        line.css('width', i + '%');
+                    }
 
-                    self.addClass(settings.classVisible);
-                }
+                    timer = setTimeout(run, settings.delay);
 
+                }, settings.delay);
+
+                self.addClass(settings.classVisible);
+            }
         });
-    }
+    };
 })(jQuery);
